3d2c7b9b5b327d36139fd1d38ec9893dc0a5c04e
paridhi-hub <74110054+paridhi-hub@users.noreply.github.com>
2020-11-12 12:22:54 +0530

    Add files via upload
    Day 01 assignment. Last two assignments of Day 01 was in HTML format, which was a complete mistake from my end. Am very sorry for that inconvinence.

---
+++ Assignment 01.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+#Question 01
+Answer is Rainbow
+
+# In[1]:
+
+
+#Question 02
+print("LETS UPGRADE")
+
+
+# In[2]:
+
+
+#Question 03
+cp = input()
+sp = input()
+if cp > sp:
+    print("Loss")
+elif cp < sp:
+    print("Profit")
+else:
+    print("Neither")
+
+
+# In[3]:
+
+
+#Question 04
+euro = input()
+rupees = int(euro) * 80
+print(rupees)
+
+
+# In[ ]:
+
+
+
+
